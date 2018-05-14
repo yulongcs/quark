ccef536b3a1532f82e6268a2bc3ae5529c822616
vdfor <jiakun.li@qq.com>
2018-05-14 14:35:36 +0800

    set zhCN as default for antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react';
+import { LocaleProvider } from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
 import App from './pages/App/index';
 import rootStore from './stores/rootStore';
 import registerServiceWorker from './registerServiceWorker';
@@ -9,7 +11,9 @@ const stores = { rootStore };
 
 ReactDOM.render(
   <Provider {...stores}>
-    <App />
+    <LocaleProvider locale={zhCN}>
+      <App />
+    </LocaleProvider>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
