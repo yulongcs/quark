52561ff1a3f47d8e76347b69a0d23acacafb4a63
vdfor <jiakun.li@qq.com>
2018-05-29 01:22:40 +0800

    rewrite HomeComponent

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,42 +1,24 @@
 import { Button } from 'antd';
-import { observable } from 'mobx';
-import { observer } from 'mobx-react';
 import * as React from 'react';
-import fetchUser from './api';
-import { actions, states } from './store/index';
 import styles from './style.module.less';
 
-interface IUser {
-  id: string | number;
-  name: string;
-}
-
-// const App: React.SFC<Iprops> = observer((Iprops) 
-@observer
 class HomeComponent extends React.Component<{}, {}> {
 
-  @observable user: IUser;
-
-  queryUser = async () => {
-    actions.home.changeTitle();
-    this.user = await fetchUser.query();
+  toCharon = () => {
+    location.href = '/index-charon.html';
   }
 
   render() {
-    // console.log(states);
-    // console.log(store.home.title);
-    // console.log(actions);
-    const name = this.user && this.user.name || 'no user';
-
     return (
-      <div>
-        <h1 className={styles.title}>{name}</h1>
-        <h1 className={styles.title}>{states.home.title}</h1>
-        <Button type="primary" onClick={this.queryUser}>获取user</Button>
+      <div className={styles.container}>
+        <div className={styles.jumbotron}>
+          <h1>charon is ready!</h1>
+          <p>charon，冥卫一。采用dark主题、侧边栏布局，定位于中后台管理应用。</p>
+          <Button type="primary" onClick={this.toCharon}>了解更多</Button>
+        </div>
       </div>
-    );
+    )
   }
-
 }
 
-export default HomeComponent; 
+export default HomeComponent;
