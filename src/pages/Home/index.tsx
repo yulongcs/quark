961d085ad5fd576d142a77f454e68e96af4ede52
vdfor <jiakun.li@qq.com>
2018-05-29 09:29:48 +0800

    split JumbotronComponent to an common component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,5 @@
-import { Button } from 'antd';
 import * as React from 'react';
-import styles from './style.module.less';
+import JumbotronComponent from '../../components/Jumbotron';
 
 class HomeComponent extends React.Component<{}, {}> {
 
@@ -10,12 +9,13 @@ class HomeComponent extends React.Component<{}, {}> {
 
   render() {
     return (
-      <div className={styles.container}>
-        <div className={styles.jumbotron}>
-          <h1>charon is ready!</h1>
-          <p>charon，冥卫一。采用dark主题、侧边栏布局，定位于中后台管理应用。</p>
-          <Button type="primary" onClick={this.toCharon}>了解更多</Button>
-        </div>
+      <div>
+        <JumbotronComponent
+          title="charon is ready!"
+          description="charon，冥卫一。采用dark主题、侧边栏布局，定位于中后台管理应用。"
+          buttonTitle="了解更多"
+          onButtonClick={this.toCharon}
+        />
       </div>
     )
   }
