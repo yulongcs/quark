22336192b85e50f151e640e4e0b97aefc742f809
vdfor <jiakun.li@qq.com>
2018-07-07 01:04:58 +0800

    update Dockerfile

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ docker stop react-sail
 docker rm react-sail
 docker rmi react-sail:1.2
 docker build -t react-sail:1.2 .
-docker run --name react-sail -p 8180:80 react-sail:1.2
+docker run --name react-sail -p 8180:80 -d react-sail:1.2
 echo 'finished'
 exit 0
