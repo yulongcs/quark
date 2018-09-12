cf652d045376302c25e8a74b21942d9107abc294
vdfor <jiakun.li@qq.com>
2018-09-12 22:59:06 +0800

    移除axios，以fetch替代

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,43 @@
+import { notification } from 'antd';
 import * as _ from 'lodash';
-import { base } from '../';
-import { codeMessage, config, getAuthCertHeaders, showErrorTip } from './common';
-import { IFetchOptions, IRequestOptions } from './types';
+import { base, credentials } from '.';
+
+interface IRequestOptions {
+  headers?: { [key: string]: string | any };
+  params?: { [key: string]: string | any };
+  data?: any;
+  method?: string;
+}
+
+interface IFetchOptions {
+  headers?: { [key: string]: string | any };
+  body?: any;
+  method?: string;
+}
+
+const config = {
+  timeout: 10000, // 指定请求超时的毫秒数(0 表示无超时时间)，如果请求花费了超过 `timeout` 的时间，请求将被中断
+  retryTimes: 2, // 请求失败时再次自动重试次数
+  retryInterval: 500 // 请求失败时再次自动重试间隔(ms)
+};
+
+const codeMessage = { // copy from https://github.com/ant-design/ant-design-pro/blob/master/src/utils/request.js
+  200: '服务器成功返回请求的数据。',
+  201: '新建或修改数据成功。',
+  202: '一个请求已经进入后台排队（异步任务）。',
+  204: '删除数据成功。',
+  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
+  401: '用户没有权限（令牌、用户名、密码错误）。',
+  403: '用户得到授权，但是访问是被禁止的。',
+  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
+  406: '请求的格式不可得。',
+  410: '请求的资源被永久删除，且不会再得到的。',
+  422: '当创建一个对象时，发生一个验证错误。',
+  500: '服务器发生错误，请检查服务器。',
+  502: '网关错误。',
+  503: '服务不可用，服务器暂时过载或维护。',
+  504: '网关超时。',
+};
 
 const request = async (retry: number, url: string, options: IFetchOptions = {}): Promise<any> => {
 
@@ -26,14 +62,17 @@ const request = async (retry: number, url: string, options: IFetchOptions = {}):
     }
 
     if (res.ok) {
-      window.postMessage('hello', '/');
+      // window.postMessage('hello', '/');
       const r = res.json();
       return typeof r === 'object' ? r : { data: r };
     }
 
     // 错误提示信息文本内容
     const errorText = res.statusText || codeMessage[res.status] || res.status;
-    showErrorTip(`http请求错误 ${res.status}`, errorText);
+    notification.error({
+      message: `http请求错误 ${res.status}`,
+      description: errorText
+    });
     console.error(res);
 
     if (res.status === 401) { // 401未授权
@@ -45,7 +84,10 @@ const request = async (retry: number, url: string, options: IFetchOptions = {}):
     retry += 1;
 
     if (retry > config.retryTimes) { // 异常自动重连
-      showErrorTip(`http请求错误 ${error && error.status || ''}`, error && error.statusText || '网络异常');
+      notification.error({
+        message: `http请求错误 ${error && error.status || ''}`,
+        description: error && error.statusText || '网络异常'
+      });
       console.error(error);
       return undefined;
     }
@@ -69,7 +111,7 @@ export default (url: string, options: IRequestOptions = {}): Promise<any> => {
 
   newOptions.headers = {
     Accept: 'application/json, text/plain, */*',
-    ...getAuthCertHeaders(),
+    Authorization: `Bearer ${credentials.token || ''}`,
     ...newOptions.headers
   };
 
--- src/utils/request/index.ts
+++
@@ -1,6 +0,0 @@
-import axios from './axios';
-import fetch from './fetch';
-
-const request = self.fetch ? fetch : axios;
-
-export default request;
