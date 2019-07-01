b3e5dd328f0e99cb095932d1ee75f0061425dfef
vdfor <jiakun.li@qq.com>
2019-07-01 17:03:10 +0800

    [Improve] 是否使用vconsole通过配置项配置；vconosle使用动态引入的方式引入

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,13 @@
 const config = {
   development: {
     history: 'browser',
-    apiBaseUrl: 'http://192.168.31.2:8182'
+    apiBaseUrl: 'http://192.168.31.2:8182',
+    useVconsole: false
   },
   production: {
     history: 'browser',
-    apiBaseUrl: 'http://192.168.31.2:8183'
+    apiBaseUrl: 'http://192.168.31.2:8183',
+    useVconsole: false
   }
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import VConsole from 'vconsole';
 import { Provider } from 'react-redux';
+import config from './config';
 import App from './page/App';
 import rootReducer from './page/App/reducer';
 import configStore from './store';
@@ -13,9 +13,12 @@ document.addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
-if (process.env.NODE_ENV === 'development') {
-  const vconsole = new VConsole();
-  console.log(`vconsole init, version is ${vconsole.version}.`);
+if (config.useVconsole) {
+  import('vconsole').then((m) => {
+    const VConsole = m.default;
+    const vconsole = new VConsole();
+    console.info(`vconsole init, version is ${vconsole.version}.`);
+  });
 }
 
 const store = configStore(rootReducer);
