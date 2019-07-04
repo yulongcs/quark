d00af1bdcbe5caf9bccddf201f3854fb7635f59e
vdfor <jiakun.li@qq.com>
2019-07-04 23:16:33 +0800

    [Improve]use mobile-detect to get device info

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './tools';
 export * from './check';
+export * from './device';
 export { default as request, ResponseBodyTypeEnum } from './request';
 export {
   default as history, goBack, goPage, jumpExternalUrl
