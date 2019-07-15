853a94b07ac44d11ffe86c412260c3cdd74c350a
vdfor <jiakun.li@qq.com>
2019-07-15 14:29:18 +0800

    [fix]大小写问题

--- src/page/app/type.ts
+++
@@ -1,9 +0,0 @@
-import { IAppBasicProps } from '../../type';
-import { IListPageProps } from '../list';
-import { ILogReportPageProps } from '../report/log-report';
-
-export interface IRootReducer {
-  appBasicReducer: IAppBasicProps;
-  listPageReducer: IListPageProps;
-  logReportPageReducer: ILogReportPageProps;
-}
