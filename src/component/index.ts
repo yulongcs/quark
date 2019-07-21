e45c46160768448ddea9c4771007c237a5adb39e
vdfor <jiakun.li@qq.com>
2019-07-21 14:44:38 +0800

    [feature]add list-skeleton component

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,3 +1,4 @@
 export { default as Route } from './Route';
 export { default as ErrorPage } from './ErrorPage';
 export * from './StyledComponents';
+export { default as ListSkeleton } from './ListSkeleton';
