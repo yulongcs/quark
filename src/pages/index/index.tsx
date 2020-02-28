d3de49f7bfaba674967a6c06c2dcc156ded81a07
vdfor <jiakun.li@qq.com>
2020-02-29 00:33:10 +0800

    [build]新增对antd-mobile的pxtovw适配

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
+import { Button } from 'antd-mobile';
 import { loadUsersApi } from './api';
+import styles from './index.scss';
 
 export default () => {
   useEffect(() => {
@@ -13,6 +15,10 @@ export default () => {
     loadUsers();
   }, []);
   return (
-    <h1>Hello, world</h1>
+    <>
+      <h1>Hello, world</h1>
+      <div className={styles.text}>你好，世界</div>
+      <Button type="primary">按钮</Button>
+    </>
   );
 };
