88afde0aa3085058ae7efacdd049c2de29e67ef4
vdfor <jiakun.li@qq.com>
2020-03-03 22:06:23 +0800

    [chore]update umi to 3.0.2

--- config/env.ts
+++ config/env.ts
@@ -1,6 +1,16 @@
-import apiConfig from './api';
 import pkg from '../package.json';
 
+// 用于应用运行的环境变量
+const APP_CONFIG = {
+  development: {
+    REACT_APP_API_BASE_URL: 'https://mock.vdfor.top/v0',
+  },
+  production: {
+    REACT_APP_API_BASE_URL: 'https://mock.vdfor.top/v0',
+  },
+};
+
+// 运行环境
 const nodeEnv = process.env.NODE_ENV || 'production';
 
 // 获取以 REACT_APP_ 开头的环境变量
@@ -8,7 +18,7 @@ const getReactAppEnvs = () => {
   const builtConstants = {
     REACT_APP_NAME: pkg.name,
     REACT_APP_VERSION: pkg.version,
-    ...apiConfig[nodeEnv],
+    ...APP_CONFIG[nodeEnv],
   };
   const initEnvs = { ...builtConstants, ...process.env };
   const envs = {};
